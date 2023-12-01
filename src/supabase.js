6e97986d0137909b8ef352e8a9d88e2eb8e5584f
Rachaphol Plookaom <124332198+naiplawan@users.noreply.github.com>
2023-12-01 10:08:53 +0700

    feat:add dotenv file

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,9 @@
 import { createClient } from "@supabase/supabase-js"
+import dotenv from "dotenv";
+dotenv.config();
 
-const supabaseUrl = "https://hkgnzkkdqwwsvccbideu.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhrZ256a2tkcXd3c3ZjY2JpZGV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDExNzU0MDMsImV4cCI6MjAxNjc1MTQwM30.OobGzJVeAqfRNP59kwd9B29m3TRBmLT3kdcQ29edyHc"
+const supabaseUrl = process.env.supabaseUrl
+const supabaseKey = process.env.supabaseKey
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
